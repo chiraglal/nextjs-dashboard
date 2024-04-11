634cff1c9b05b856a4abe22c9738769ecf5e56a4
CHIRAG LAL <chiraglal15@gmail.com>
2024-04-11 13:16:57 +0530

    minor changes

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Sidebar from "./components/Sidebar";
 export default function App({ Component, pageProps }) {
   return (
     <Sidebar>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Sidebar>
   );
 }
